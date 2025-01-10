300c5bd2c46f6d4a9b47af21deca779184a7b02b
Julien QUENTIN <jqntn88@gmail.com>
2025-01-10 23:22:36 +0100

    init

---
+++ main.cpp
@@ -0,0 +1,63 @@
+#include <corecrt.h>
+#include <cstdio>
+#include <cstdlib>
+#include <filesystem>
+#include <string>
+
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+
+static errno_t e = 0;
+
+static void
+start_python()
+{
+  Py_Initialize();
+}
+
+static void
+stop_python()
+{
+  e = Py_FinalizeEx();
+  if (e) {
+    std::printf("ABORT | Py_FinalizeEx | errno: %d", e);
+    std::abort();
+  }
+}
+
+static void
+run_python_script(const std::filesystem::path& script_path)
+{
+  std::string script_path_str = script_path.generic_string();
+  std::string script_stem = script_path.stem().string();
+
+  FILE* p_file;
+  e = fopen_s(&p_file, script_path_str.c_str(), "r");
+  if (e) {
+    std::printf("ABORT | fopen_s | %s | errno: %d", script_path_str.c_str(), e);
+    std::abort();
+  }
+
+  e = PyRun_SimpleFileExFlags(p_file, script_stem.c_str(), 1, nullptr);
+  if (e) {
+    std::printf("ABORT | PyRun_SimpleFileExFlags | %s | errno: %d",
+                script_path_str.c_str(),
+                e);
+    std::abort();
+  }
+}
+
+int
+main(int argc, char** argv)
+{
+  std::string script_name = "hello.py";
+
+  std::filesystem::path exe_path = argv[0];
+  std::filesystem::path script_path = exe_path.parent_path() / script_name;
+
+  start_python();
+  run_python_script(script_path);
+  stop_python();
+
+  return EXIT_SUCCESS;
+}

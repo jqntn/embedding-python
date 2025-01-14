54de08494940c3540675c881af6013013c7596a6
Julien QUENTIN <jqntn88@gmail.com>
2025-01-14 23:35:03 +0100

    fix pip install

--- pybind/main.cpp
+++ pybind/main.cpp
@@ -1,6 +1,7 @@
 #include <cstdlib>
 #include <exception>
 #include <filesystem>
+#include <format>
 #include <iostream>
 #include <pybind11/embed.h>
 
@@ -13,20 +14,37 @@ main(int argc, char** argv)
 {
   g_app_path = std::filesystem::path(argv[0]).parent_path();
 
-  py::scoped_interpreter guard;
-
   if (!std::filesystem::exists(g_app_path / "Lib/site-packages/")) {
-    try {
-      py::exec(R"(
+    {
+      py::scoped_interpreter guard;
+      try {
+        py::exec(
+          R"(
 import urllib.request
 exec(urllib.request.urlopen("https://bootstrap.pypa.io/get-pip.py").read())
-      )");
-    } catch (const std::exception& e) {
-      std::cout << e.what() << std::endl;
+          )");
+      } catch (const std::exception& e) {
+        std::cout << e.what() << '\n';
+      }
     }
+    {
+      py::scoped_interpreter guard;
+      try {
+        py::exec(std::format(
+          R"(
+import pip
+pip.main(["install", "-r", "{}"])
+          )",
+          (g_app_path / "requirements.txt").generic_string()));
+      } catch (const std::exception& e) {
+        std::cout << e.what() << '\n';
+      }
+    }
+    std::filesystem::remove_all(g_app_path / "Scripts/");
   }
 
-  /*
+  py::scoped_interpreter guard;
+
   try {
     py::exec(R"(
       import numpy as np
@@ -37,10 +55,8 @@ exec(urllib.request.urlopen("https://bootstrap.pypa.io/get-pip.py").read())
       bpy.ops.object.select_all(action="DESELECT")
     )");
   } catch (const std::exception& e) {
-    std::cout << e.what();
-    throw;
+    std::cout << e.what() << '\n';
   }
-  */
 
   return EXIT_SUCCESS;
 }
